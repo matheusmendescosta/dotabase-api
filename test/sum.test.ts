944e28e291f2ebba783f399f5915f44d6e951718
matheusmendescosta <matheus_mendess@live.com>
2024-08-27 21:12:29 -0300

    setup test

---
+++ test/sum.test.ts
@@ -0,0 +1,9 @@
+describe("GetHeroesService", () => {
+  it("should return a list of heroes", async () => {
+    const a = 1;
+    const b = 2;
+    const result = a + b;
+
+    expect(result).toEqual(3);
+  });
+});
